6bdafdfda4f5b76a62584391e80f856e0f2fff3b
mannybennett <mannybennett99@gmail.com>
2023-03-30 14:54:37 -0400

    finished

--- controllers/users.js
+++ controllers/users.js
@@ -1,17 +1,43 @@
-const users = require('../data/index')
-const sampleUser = require('../data/sampleUser')
+const users = require('../data/index');
+const sampleUser = require('../data/sampleUser');
 
 module.exports = {
   listUsers: (req, res) => res.json(users),
-  showUser: (req, res) => res.json(users.find(user => user.id === parseInt(req.params.id))),
+
+  showUser: (req, res) => {
+    let currentUser = users.find(user => user.id === parseInt(req.params.id));
+    if (!currentUser) {
+      res.status(404).send('User not found.');
+    }
+    return res.json(currentUser)
+  },
+
   createUser: (req, res) => {
     sampleUser.id = users.length + 1;
     users.push(sampleUser);
     res.json(sampleUser);
   },
+
   updateUser: (req, res) => {
-    currentUser = users.find(user => user.id === parseInt(req.params.id));
-    res.json(req.body)
+    let userId = parseInt(req.params.id);
+    let currentUser = users.find(user => user.id === userId);
+    if (!currentUser) {
+      res.status(400).send('User not found.');
+    } else {
+      currentUser = {...currentUser, ...req.body};
+      users.splice(userId - 1, 1, currentUser);
+      return res.json(currentUser);
+    }
+  },
+  
+  deleteUser: (req, res) => {
+    let userId = parseInt(req.params.id);
+    let currentUser = users.find(user => user.id === userId);
+    if (!currentUser) {
+      res.status(400).send('User not found.');
+    } else {
+      users.splice(userId - 1, 1);
+      return res.send('User deleted successfully.');
+    }
   }
-  // deleteUser:
 }
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,6 @@ router.get('/users', usersController.listUsers)
 router.get('/users/:id', usersController.showUser)
 router.post('/users', usersController.createUser)
 router.put('/users/:id', usersController.updateUser)
+router.delete('/users/:id', usersController.deleteUser)
 
 module.exports = router
